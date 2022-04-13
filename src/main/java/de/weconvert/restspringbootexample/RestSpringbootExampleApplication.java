a00d27dc11200eb31dc45eed647288973523679a
MartinHein-dev <martinhein86@gmx.de>
2022-04-13 14:54:01 +0200

    init

---
+++ src/main/java/de/weconvert/restspringbootexample/RestSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package de.weconvert.restspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/weconvert/restspringbootexample/RestSpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.weconvert.restspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
