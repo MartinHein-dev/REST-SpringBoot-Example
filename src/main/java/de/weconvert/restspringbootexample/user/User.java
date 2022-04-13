a59f8fa582c20e740aa52bbfbcd45fc4f5572c0a
MartinHein-dev <martinhein86@gmx.de>
2022-04-13 16:33:34 +0200

    + User Validation / + Validation Error Response

--- src/main/java/de/weconvert/restspringbootexample/user/User.java
+++ src/main/java/de/weconvert/restspringbootexample/user/User.java
@@ -2,10 +2,17 @@ package de.weconvert.restspringbootexample.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+	
+	@Size(min = 2, message = "Name shoud have atleast 2 characters")
 	private String name;
+	
+	@Past
 	private Date birthDate;
 	
 	public User(Integer id, String name, Date birthDate) {
