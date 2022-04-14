75f0af06e9d6c3a2cf815a46bf6d534abf1f6ad7
MartinHein-dev <martinhein86@gmx.de>
2022-04-14 15:44:59 +0200

    h2, jpa, bootstrap data, dynamische User + Posts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user VALUES(10001, sysdate(), 'Klaus');
+INSERT INTO user VALUES(10002, sysdate(), 'Maria');
+INSERT INTO user VALUES(10003, sysdate(), 'Karl');
+INSERT INTO user VALUES(10004, sysdate(), 'Maja');
+INSERT INTO user VALUES(10005, sysdate(), 'Jam');
+
+INSERT INTO post VALUES(11001, 'My First Post', 10001);
+INSERT INTO post VALUES(11002, 'My Second Post', 10001);
