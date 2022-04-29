fc52f8a65e09d68b9f0dbf15ee6d5be3de05786d
MartinHein-dev <martinhein86@gmx.de>
2022-04-29 15:42:56 +0200

    test von git

--- src/main/java/de/weconvert/restspringbootexample/RestSpringbootExampleApplication.java
+++ src/main/java/de/weconvert/restspringbootexample/RestSpringbootExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestSpringbootExampleApplication {
 
 	
-	//Test Kommentar
+	//Test Kommentar und wieder ein update
 	public static void main(String[] args) {
 		SpringApplication.run(RestSpringbootExampleApplication.class, args);
 	}
