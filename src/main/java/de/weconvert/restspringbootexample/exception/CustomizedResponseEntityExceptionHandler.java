b5900de40d960d12647d918d7e00b0d2a43aa48d
MartinHein-dev <martinhein86@gmx.de>
2022-05-12 15:34:57 +0200

    own NOT_FOUND & METHOD_NOT_ALLOWED Exception added

--- src/main/java/de/weconvert/restspringbootexample/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/de/weconvert/restspringbootexample/exception/CustomizedResponseEntityExceptionHandler.java
@@ -5,11 +5,13 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import de.weconvert.restspringbootexample.user.UserNotFoundException;
@@ -27,6 +29,28 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	
 	@ExceptionHandler(UserNotFoundException.class)
 	public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.METHOD_NOT_ALLOWED);
+	}
+
+	/**
+	 * Folgende Einträge müssen in die application.properties aufgenommen werden, dass diese Funktion greift
+	 * 
+	 * spring.mvc.throw-exception-if-no-handler-found=true
+	 * spring.web.resources.add-mappings=false
+	 */
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
